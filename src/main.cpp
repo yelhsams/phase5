4a4ccef9ce4462b6ba9e228866fd70da4d957ff7
Ashley Sheng <51773182+yelhsams@users.noreply.github.com>
2025-12-05 17:54:34 -0500

    i dont know what this is for

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,25 @@
 #include <algorithm>
 #include <iostream>
 
-static std::string read_istream(std::istream &is) {
+static std::string read_istream(std::istream &is)
+{
   return std::string(std::istreambuf_iterator<char>(is),
                      std::istreambuf_iterator<char>());
 }
 
-static bool has_opt(const Command &cmd, const std::string &name) {
-  auto present = [&](const std::string &needle) {
+static bool has_opt(const Command &cmd, const std::string &name)
+{
+  auto present = [&](const std::string &needle)
+  {
     return std::find(cmd.opt.begin(), cmd.opt.end(), needle) != cmd.opt.end();
   };
   return present(name);
 }
 
-static std::string token_kind_name(const mitscript::Token &t) {
-  switch (t.kind) {
+static std::string token_kind_name(const mitscript::Token &t)
+{
+  switch (t.kind)
+  {
   case mitscript::TokenKind::STRING:
     return "STRINGLITERAL";
   case mitscript::TokenKind::INT:
@@ -48,7 +53,8 @@ static std::string token_kind_name(const mitscript::Token &t) {
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   Command command = cli_parse(argc, argv);
 
   std::string contents = read_istream(*command.input_stream);
@@ -56,80 +62,80 @@ int main(int argc, char **argv) {
   std::string output_filename = command.output_filename;
 
   bool had_error = false;
-  switch (command.kind) {
-  case CommandKind::SCAN: {
-    mitscript::Lexer lexer(contents);
-    std::vector<mitscript::Token> tokens = lexer.lex();
+  switch (command.kind)
+  {
+  case CommandKind::SCAN:
+  {
+    try
+    {
+      mitscript::Lexer lexer(contents);
+      std::vector<mitscript::Token> tokens = lexer.lex();
 
-    std::ostream &out = *command.output_stream;
+      std::ostream &out = *command.output_stream;
 
-    for (const auto &token : tokens) {
-      if (token.kind == mitscript::TokenKind::ERROR) {
-        had_error = true;
-        std::cerr << "Error from lexer at line " << token.start_line
-                  << ", column " << token.start_col << ": " << token.text
-                  << "\n";
-        continue;
-      }
+      // for (const auto &token : tokens)
+      // {
+      //   if (token.kind == mitscript::TokenKind::ERROR)
+      //   {
+      //     had_error = true;
+      //     std::cerr << "Error from lexer at line " << token.start_line
+      //               << ", column " << token.start_col << ": " << token.text
+      //               << "\n";
+      //     continue;
+      //   }
 
-      const std::string kind_name = token_kind_name(token);
+      //   const std::string kind_name = token_kind_name(token);
 
-      if (token.kind == mitscript::TokenKind::EOF_TOKEN) {
-        out << token.start_line << ' ' << kind_name << '\n';
-      } else if (!kind_name.empty()) {
-        out << token.start_line << ' ' << kind_name << ' ' << token.text
-            << '\n';
-      } else {
-        out << token.start_line << ' ' << token.text << '\n';
-      }
+      //   if (token.kind == mitscript::TokenKind::EOF_TOKEN)
+      //   {
+      //     out << token.start_line << ' ' << kind_name << '\n';
+      //   }
+      //   else if (!kind_name.empty())
+      //   {
+      //     out << token.start_line << ' ' << kind_name << ' ' << token.text
+      //         << '\n';
+      //   }
+      //   else
+      //   {
+      //     out << token.start_line << ' ' << token.text << '\n';
+      //   }
+      // }
+      break;
+    }
+    catch (const std::exception &e)
+    {
+      had_error = true;
+      std::cerr << e.what() << "\n";
     }
     break;
   }
+  case CommandKind::PARSE:
+  {
+    try
+    {
+      mitscript::Lexer lexer(contents);
+      std::vector<mitscript::Token> tokens = lexer.lex();
 
-  case CommandKind::PARSE: {
-    mitscript::Lexer lexer(contents);
-    std::vector<mitscript::Token> tokens = lexer.lex();
-
-    for (const auto &token : tokens) {
-      if (token.kind == mitscript::TokenKind::ERROR) {
-        had_error = true;
-        std::cerr << "Error from lexer at line " << token.start_line
-                  << ", column " << token.start_col << ": " << token.text
-                  << "\n";
-      }
-    }
-    if (had_error)
-      break;
-
-    try {
       mitscript::Parser parser(tokens);
       auto ast = parser.parse();
 
       std::cout << "Parse successful\n";
-
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
     }
     break;
   }
 
-  case CommandKind::COMPILE: {
-    mitscript::Lexer lexer(contents);
-    std::vector<mitscript::Token> tokens = lexer.lex();
+  case CommandKind::COMPILE:
+  {
+    try
+    {
+      mitscript::Lexer lexer(contents);
+      std::vector<mitscript::Token> tokens = lexer.lex();
 
-    for (const auto &token : tokens) {
-      if (token.kind == mitscript::TokenKind::ERROR) {
-        had_error = true;
-        std::cerr << "Error from lexer at line " << token.start_line
-                  << ", column " << token.start_col << ": " << token.text
-                  << "\n";
-      }
-    }
-    if (had_error)
-      break;
-
-    try {
       mitscript::Parser parser(tokens);
       auto ast = parser.parse();
 
@@ -138,7 +144,8 @@ int main(int argc, char **argv) {
       ast->accept(&cfg_builder);
 
       // Optional optimization: constant propagation / folding.
-      if (has_opt(command, "constprop")) {
+      if (has_opt(command, "constprop"))
+      {
         mitscript::analysis::ConstantPropagation cp(cfg);
         cp.run();
         cp.rewrite();
@@ -147,7 +154,9 @@ int main(int argc, char **argv) {
       BytecodeConverter bc;
       bytecode::Function *bytecode = bc.convert(cfg, /*is_toplevel=*/true);
       bytecode::prettyprint(bytecode, *command.output_stream);
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
       std::cout << "Compilation error:\n";
       had_error = true;
       std::cerr << e.what() << "\n";
@@ -155,8 +164,10 @@ int main(int argc, char **argv) {
 
     break;
   }
-  case CommandKind::DERBY: {
-    try {
+  case CommandKind::DERBY:
+  {
+    try
+    {
       // Compile source to bytecode and immediately execute on the VM.
       mitscript::Lexer lexer(contents);
       std::vector<mitscript::Token> tokens = lexer.lex();
@@ -169,101 +180,108 @@ int main(int argc, char **argv) {
       ast->accept(&cfg_builder);
 
       // Optional optimization: constant propagation / folding.
-      // if (has_opt(command, "constprop") || has_opt(command, "all")) {
-      // // if (true) {
-      //   mitscript::analysis::ConstantPropagation cp(cfg);
-      //   cp.run();
-      //   cp.rewrite();
-      // }
+      if (has_opt(command, "constprop") || has_opt(command, "all"))
+      {
+        // if (true) {
+        mitscript::analysis::ConstantPropagation cp(cfg);
+        cp.run();
+        cp.rewrite();
+      }
 
       BytecodeConverter bc;
       bytecode::Function *bytecode = bc.convert(cfg, /*is_toplevel=*/true);
 
       // Optional optimization: constant propagation on bytecode
-      if (has_opt(command, "bytecode-constprop") || has_opt(command, "all")) {
+      if (has_opt(command, "bytecode-constprop") || has_opt(command, "all"))
+      {
         bytecode::opt_constprop::constant_propagate(bytecode);
       }
 
       // Optional optimization: dead code elimination (runs after constant
       // propagation)
-      if (has_opt(command, "deadcode") || has_opt(command, "all")) {
+      if (has_opt(command, "deadcode") || has_opt(command, "all"))
+      {
         bytecode::opt_deadcode::eliminate_dead_code(bytecode);
       }
 
       // Optional optimization: peephole (constant folding)
-      if (has_opt(command, "peephole") || has_opt(command, "all")) {
+      if (has_opt(command, "peephole") || has_opt(command, "all"))
+      {
         bytecode::opt_peephole::peephole_optimize(bytecode);
       }
 
-      if (has_opt(command, "inline")) {
+      if (has_opt(command, "inline"))
+      {
         bytecode::opt_inline::inline_functions(bytecode);
       }
-      if (has_opt(command, "licm")) {
+      if (has_opt(command, "licm"))
+      {
         bytecode::opt_licm::run(bytecode);
       }
       vm::VM vm(command.mem);
       vm.run(bytecode);
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
     }
     break;
   }
-  case CommandKind::INTERPRET: {
-    mitscript::Lexer lexer(contents);
-    std::vector<mitscript::Token> tokens = lexer.lex();
+  case CommandKind::INTERPRET:
+  {
+    try
+    {
+      mitscript::Lexer lexer(contents);
+      std::vector<mitscript::Token> tokens = lexer.lex();
 
-    for (const auto &token : tokens) {
-      if (token.kind == mitscript::TokenKind::ERROR) {
-        had_error = true;
-        std::cerr << "Error from lexer at line " << token.start_line
-                  << ", column " << token.start_col << ": " << token.text
-                  << "\n";
-      }
-    }
-    if (had_error)
-      break;
-
-    try {
       mitscript::Parser parser(tokens);
       auto ast = parser.parse();
 
       mitscript::interpret(*ast);
-
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
     }
     break;
   }
 
-  case CommandKind::VM: {
-    try {
+  case CommandKind::VM:
+  {
+    try
+    {
       // Parse bytecode
       bytecode::Function *bytecode_func = bytecode::parse(contents);
 
       size_t max_mem_mb = command.mem;
 
       // Optional optimization: constant propagation on bytecode
-      if (has_opt(command, "bytecode-constprop") || has_opt(command, "all")) {
+      if (has_opt(command, "bytecode-constprop") || has_opt(command, "all"))
+      {
         bytecode::opt_constprop::constant_propagate(bytecode_func);
       }
 
       // Optional optimization: dead code elimination (runs after constant
       // propagation)
-      if (has_opt(command, "deadcode") || has_opt(command, "all")) {
+      if (has_opt(command, "deadcode") || has_opt(command, "all"))
+      {
         bytecode::opt_deadcode::eliminate_dead_code(bytecode_func);
       }
 
       // Optional optimization: peephole (constant folding)
-      if (has_opt(command, "peephole") || has_opt(command, "all")) {
+      if (has_opt(command, "peephole") || has_opt(command, "all"))
+      {
         bytecode::opt_peephole::peephole_optimize(bytecode_func);
       }
 
-      if (has_opt(command, "inline") || has_opt(command, "all")) {
+      if (has_opt(command, "inline") || has_opt(command, "all"))
+      {
         bytecode::opt_inline::inline_functions(bytecode_func);
       }
-      if (has_opt(command, "licm") || has_opt(command, "all")) {
+      if (has_opt(command, "licm") || has_opt(command, "all"))
+      {
         bytecode::opt_licm::run(bytecode_func);
       }
 
@@ -273,28 +291,41 @@ int main(int argc, char **argv) {
 
       // Cleanup
       delete bytecode_func;
-
-    } catch (const std::runtime_error &e) {
+    }
+    catch (const std::runtime_error &e)
+    {
       // Catch lexer errors early and return
       had_error = true;
       std::cerr << e.what() << "\n";
       return had_error ? 1 : 0;
-    } catch (const vm::InsufficientStackException &e) {
+    }
+    catch (const vm::InsufficientStackException &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
-    } catch (const vm::UninitializedVariableException &e) {
+    }
+    catch (const vm::UninitializedVariableException &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
-    } catch (const vm::IllegalCastException &e) {
+    }
+    catch (const vm::IllegalCastException &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
-    } catch (const vm::IllegalArithmeticException &e) {
+    }
+    catch (const vm::IllegalArithmeticException &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
-    } catch (const vm::RuntimeException &e) {
+    }
+    catch (const vm::RuntimeException &e)
+    {
       had_error = true;
       std::cerr << e.what() << "\n";
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
       had_error = true;
       std::cerr << "Error: " << e.what() << "\n";
     }
